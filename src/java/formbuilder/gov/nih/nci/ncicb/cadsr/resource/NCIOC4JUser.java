f43fdc9c86cb33f179dc326ed727442774120ff1
kakkodis <kakkodis>
2004-01-23 15:47:37 +0000

    Add all
    SVN-Revision: 21

--- src/java/formbuilder/gov/nih/nci/ncicb/cadsr/resource/NCIOC4JUser.java
+++
@@ -1,9 +0,0 @@
-package gov.nih.nci.ncicb.cadsr.resource;
-import gov.nih.nci.ncicb.cadsr.resource.NCIUser;
-import com.evermind.security.User;
-import gov.nih.nci.ncicb.cadsr.security.oc4j.BaseUserManager;
-public interface NCIOC4JUser extends NCIUser,User
-{
-  public BaseUserManager getUserManager();
-  public void setUserManager(BaseUserManager newUserManager);
-}
--- src/java/formbuilder/gov/nih/nci/ncicb/cadsr/security/NCIUserObject.java
+++
@@ -1,9 +0,0 @@
-package gov.nih.nci.ncicb.cadsr.security;
-import gov.nih.nci.ncicb.cadsr.resource.NCIUser;
-
-public class NCIUserObject  implements NCIUser
-{
-  public NCIUserObject()
-  {
-  }
-}
