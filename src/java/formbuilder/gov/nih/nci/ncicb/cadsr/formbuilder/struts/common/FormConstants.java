f92219c44a843ba09ef5dff5a2a815b1b25c73c2
kakkodis <kakkodis>
2004-02-12 22:12:08 +0000

    Add all
    SVN-Revision: 65

--- src/java/formbuilder/gov/nih/nci/ncicb/cadsr/formbuilder/struts/common/FormConstants.java
+++ src/java/formbuilder/gov/nih/nci/ncicb/cadsr/formbuilder/struts/common/FormConstants.java
@@ -1,6 +1,6 @@
 package gov.nih.nci.ncicb.cadsr.formbuilder.struts.common;
 
-public interface FormConstants 
+public interface FormConstants
 {
     public static final String FORM_LONG_NAME = "formLongName";
     public static final String CONTEXT_ID_SEQ = "contextIdSeq";
@@ -9,14 +9,16 @@ public interface FormConstants
     public static final String FORM_SEARCH_RESULTS = "FormSearchResults";
     public static final String PROTOCOL_ID_SEQ = "protocolIdSeq";
     public static final String CATEGORY_NAME = "categoryName";
-    public static final String FORM_SEARCH_RESULTS_PAGENATION="FormSearchResultsPagenation";   
-    public static final String PAGEINDEX = "PageIndex"; 
-    
-    public static final String CONTEXTS = "Contexts";
-    public static final String WORKFLOWS = "Workflows";
-    public static final String PROTOCOLS = "Protocols";
+    public static final String FORM_SEARCH_RESULTS_PAGENATION="formSearchResultsPagenation";
+    public static final String PAGEINDEX = "pageIndex";
+
+    public static final String ALL_CONTEXTS = "allContexts";
+    public static final String ALL_WORKFLOWS = "allWorkflows";
+    public static final String ALL_PROTOCOLS = "allProtocols";
 
     public static final String PROTOCOLS_LOV_ID_FIELD = "protocolIdSeq";
-    public static final String PROTOCOLS_LOV_NAME_FIELD = "protocolLongName";    
-    
+    public static final String PROTOCOLS_LOV_NAME_FIELD = "protocolLongName";
+
+
+
 }
