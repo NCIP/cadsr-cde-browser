05cfa1af0bbc546163a49f3c36426ed37ae1c34d
kimh <kimh>
2004-02-13 17:37:18 +0000

    *** empty log message ***
    SVN-Revision: 75

--- src/java/formbuilder/gov/nih/nci/ncicb/cadsr/persistence/dao/jdbc/JDBCBaseDAO.java
+++ src/java/formbuilder/gov/nih/nci/ncicb/cadsr/persistence/dao/jdbc/JDBCBaseDAO.java
@@ -8,18 +8,16 @@ import gov.nih.nci.ncicb.cadsr.security.oc4j.BaseUserManager;
 import gov.nih.nci.ncicb.cadsr.servicelocator.ServiceLocator;
 import gov.nih.nci.ncicb.cadsr.servicelocator.SimpleServiceLocator;
 import gov.nih.nci.ncicb.cadsr.util.StringUtils;
-
-import org.apache.commons.logging.LogFactory;
-
+import org.springframework.jdbc.object.StoredProcedure;
+import org.springframework.jdbc.core.SqlParameter;
+import org.springframework.jdbc.core.SqlOutParameter;
 import org.springframework.jdbc.object.SqlFunction;
-
+import org.apache.commons.logging.LogFactory;
 import java.sql.Connection;
 import java.sql.SQLException;
 import java.sql.Types;
-
 import java.util.HashMap;
 import java.util.Map;
-
 import javax.sql.DataSource;
 
 
@@ -65,39 +63,135 @@ public class JDBCBaseDAO extends BaseDAO implements PersistenceConstants {
     return dsKey;
   }
 
+  /**
+   * Check if the user has a create privilege on the administered component
+   * within the context
+   * 
+   * @param username corresponds to the login user name.
+   * @param acType corresponds to the administered component type
+   * @param conteIdseq corresponds to the context id seq 
+   * @return <b>boolean</b> true indicates that user has an create
+   *         privilege and false when the user has not
+   */
   public boolean hasCreate(
     String username,
     String acType,
     String conteIdseq) {
-    int[] inputTypes = { Types.VARCHAR, Types.VARCHAR, Types.VARCHAR };
-    String sqlStmt =
-      " SELECT cadsr_security_util.has_create_privilege(?,?,?) FROM DUAL ";
-    SqlFunction hasCreateFunc =
-      new SqlFunction(this.getDataSource(), sqlStmt, inputTypes, Types.VARCHAR);
-    hasCreateFunc.compile();
-
-    Object[] inputValues = { username, acType, conteIdseq };
-    String retVal = (String) hasCreateFunc.runGeneric(inputValues);
-
-    return StringUtils.toBoolean(retVal);
+    
+		HasCreateQuery hasCreate = new HasCreateQuery(this.getDataSource());
+		String retValue = hasCreate.execute(username, acType, conteIdseq);
+		//System.out.println("Stored Function: " + retValue + ".");
+    return StringUtils.toBoolean(retValue);
   }
+    
+	private class HasCreateQuery extends StoredProcedure {
+	  // inner class to handle to call the stored function
+		public HasCreateQuery(DataSource ds) {
+			super(ds, "cadsr_security_util.has_create_privilege");
+			setFunction(true);
+			declareParameter(new SqlOutParameter("returnValue", Types.VARCHAR));
+			declareParameter(new SqlParameter("p_ua_name", Types.VARCHAR));
+      declareParameter(new SqlParameter("p_actl_name", Types.VARCHAR));
+      declareParameter(new SqlParameter("p_conte_idseq", Types.VARCHAR));
+			compile();
+		}
+	
+		public String execute(String username, String acType, String conteIdseq) {
+			Map in = new HashMap();
+			in.put("p_ua_name", username);
+			in.put("p_actl_name", acType);
+			in.put("p_conte_idseq", conteIdseq);
+			Map out = execute(in);
+			String retValue = (String) out.get("returnValue");
+			return retValue;
+		}
+	}
 
+  /**
+   * Check if the user has a delete privilege on the administered component
+   * within the context
+   * 
+   * @param username corresponds to the login user name.
+   * @param acIdseq corresponds to the administered component id seq 
+   * @return <b>boolean</b> true indicates that user has an delete
+   *         privilege and false when the user has not
+   */
   public boolean hasDelete(
     String username,
     String acIdseq) {
-    int[] inputTypes = { Types.VARCHAR, Types.VARCHAR };
-    String sqlStmt =
-      " SELECT cadsr_security_util.has_delete_privilege(?,?) FROM DUAL ";
-    SqlFunction hasDeleteFunc =
-      new SqlFunction(this.getDataSource(), sqlStmt, inputTypes, Types.VARCHAR);
-    hasDeleteFunc.compile();
-
-    Object[] inputValues = { username, acIdseq };
-    String retVal = (String) hasDeleteFunc.runGeneric(inputValues);
-
-    return StringUtils.toBoolean(retVal);
+    
+		HasDeleteQuery hasDelete = new HasDeleteQuery(this.getDataSource());
+		String retValue = hasDelete.execute(username, acIdseq);
+		System.out.println("Stored Function: " + retValue + ".");
+    return StringUtils.toBoolean(retValue);
   }
+    
+	private class HasDeleteQuery extends StoredProcedure {
+	  // inner class to handle to call the stored function
+	
+		public HasDeleteQuery(DataSource ds) {
+			super(ds, "cadsr_security_util.has_delete_privilege");
+			setFunction(true);
+			declareParameter(new SqlOutParameter("returnValue", Types.VARCHAR));
+			declareParameter(new SqlParameter("p_ua_name", Types.VARCHAR));
+      declareParameter(new SqlParameter("p_ac_idseq", Types.VARCHAR));
+			compile();
+		}
+	
+		public String execute(String username, String acIdseq) {
+			Map in = new HashMap();
+			in.put("p_ua_name", username);
+			in.put("p_ac_idseq", acIdseq);
+			Map out = execute(in);
+			String retValue = (String) out.get("returnValue");
+			return retValue;
+			
+		}
+	}
 
+  /**
+   * Check if the user has a update privilege on the administered component
+   * within the context
+   * 
+   * @param username corresponds to the login user name.
+   * @param acIdseq corresponds to the administered component id seq 
+   * @return <b>boolean</b> true indicates that user has an update
+   *         privilege and false when the user has not
+   */
+  public boolean hasUpdate(
+    String username,
+    String acIdseq) {
+    
+		HasUpdateQuery hasUpdate = new HasUpdateQuery(this.getDataSource());
+		String retValue = hasUpdate.execute(username, acIdseq);
+		//System.out.println("Stored Function: " + retValue + ".");
+    return StringUtils.toBoolean(retValue);
+  }
+    
+	private class HasUpdateQuery extends StoredProcedure {
+	  // inner class to handle to call the stored function
+	
+		public HasUpdateQuery(DataSource ds) {
+			super(ds, "cadsr_security_util.has_update_privilege");
+			setFunction(true);
+			declareParameter(new SqlOutParameter("returnValue", Types.VARCHAR));
+			declareParameter(new SqlParameter("p_ua_name", Types.VARCHAR));
+      declareParameter(new SqlParameter("p_ac_idseq", Types.VARCHAR));
+			compile();
+		}
+	
+		public String execute(String username, String acIdseq) {
+			Map in = new HashMap();
+			in.put("p_ua_name", username);
+			in.put("p_ac_idseq", acIdseq);
+			Map out = execute(in);
+			String retValue = (String) out.get("returnValue");
+			return retValue;
+			
+		}
+	}
+
+/*
   public boolean hasUpdate(
     String username,
     String acIdseq) {
@@ -113,6 +207,8 @@ public class JDBCBaseDAO extends BaseDAO implements PersistenceConstants {
 
     return StringUtils.toBoolean(retVal);
   }
+
+*/
 
   public static void main(String[] args) {
     ServiceLocator locator = new SimpleServiceLocator();
