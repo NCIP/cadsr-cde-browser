e231b724df505035a41c478ec7ba05ee5f40c7dc
chilukur <chilukur>
2004-02-13 21:20:28 +0000

    *** empty log message ***
    SVN-Revision: 79

--- src/java/formbuilder/gov/nih/nci/ncicb/cadsr/persistence/dao/QuestionDAO.java
+++ src/java/formbuilder/gov/nih/nci/ncicb/cadsr/persistence/dao/QuestionDAO.java
@@ -1,4 +1,86 @@
 package gov.nih.nci.ncicb.cadsr.persistence.dao;
 
-public interface QuestionDAO  {
+import gov.nih.nci.ncicb.cadsr.exception.DMLException;
+import gov.nih.nci.ncicb.cadsr.resource.Question;
+
+import java.util.Collection;
+
+
+public interface QuestionDAO {
+  /**
+   * Gets all the valid values for a question
+   *
+   * @param <b>questionId</b> Idseq of the question component.
+   *
+   * @return <b>Collection</b> List of form valid value objects
+   */
+  public Collection getValidValues(String questionId);
+
+  /**
+   * Creates a new question component (just the header info).
+   *
+   * @param <b>newQuestion</b> Question object
+   *
+   * @return <b>Question</b> Question object representing the new question.
+   *
+   * @throws <b>DMLException</b>
+   */
+  public Question createQuestionComponent(Question newQuestion)
+    throws DMLException;
+
+  /**
+   * Adds new valid values to a question.
+   *
+   * @param <b>questionId</b> Idseq of the question component.
+   * @param <b>validValues</b> FormValidValue objects to be added to the
+   *        question
+   *
+   * @return <b>Question</b> Question object representing the question after
+   *         addition of new validValues.
+   *
+   * @throws <b>DMLException</b>
+   */
+  public Question addValidValues(
+    String questionId,
+    Collection validValues) throws DMLException;
+
+  /**
+   * Deletes the specified question and all its associated components.
+   *
+   * @param <b>questionId</b> Idseq of the question component.
+   *
+   * @return <b>int</b> 1 - success, 0 - failure.
+   *
+   * @throws <b>DMLException</b>
+   */
+  public int deleteQuestion(String questionId) throws DMLException;
+
+  /**
+   * Changes the display order of the specified question. Display order of the
+   * other questions the module is also updated accordingly.
+   *
+   * @param <b>questionId</b> Idseq of the question component.
+   * @param <b>newDisplayOrder</b> New display order of the question component.
+   *
+   * @return <b>int</b> 1 - success, 0 - failure.
+   *
+   * @throws <b>DMLException</b>
+   */
+  public int updateDisplayOrder(
+    String questionId,
+    int newDisplayOrder) throws DMLException;
+
+  /**
+   * Changes the long name of a question.
+   *
+   * @param <b>questionId</b> Idseq of the question component.
+   * @param <b>newLongName</b> New long name of the question component.
+   *
+   * @return <b>int</b> 1 - success, 0 - failure.
+   *
+   * @throws <b>DMLException</b>
+   */
+  public int updateQuestionLongName(
+    String questionId,
+    String newLongName) throws DMLException;
 }
--- src/java/formbuilder/gov/nih/nci/ncicb/cadsr/persistence/dao/jdbc/JDBCQuestionDAO.java
+++ src/java/formbuilder/gov/nih/nci/ncicb/cadsr/persistence/dao/jdbc/JDBCQuestionDAO.java
@@ -1,9 +1,46 @@
 package gov.nih.nci.ncicb.cadsr.persistence.dao.jdbc;
+
+import gov.nih.nci.ncicb.cadsr.exception.DMLException;
 import gov.nih.nci.ncicb.cadsr.persistence.dao.QuestionDAO;
+import gov.nih.nci.ncicb.cadsr.resource.Question;
 import gov.nih.nci.ncicb.cadsr.servicelocator.ServiceLocator;
 
-public class JDBCQuestionDAO extends JDBCBaseDAO implements QuestionDAO  {
+import java.util.Collection;
+
+
+public class JDBCQuestionDAO extends JDBCBaseDAO implements QuestionDAO {
   public JDBCQuestionDAO(ServiceLocator locator) {
-    super(locator);  
+    super(locator);
+  }
+
+  public Collection getValidValues(String questionId) {
+    return null;
+  }
+
+  public Question createQuestionComponent(Question newQuestion)
+    throws DMLException {
+    return null;
+  }
+
+  public Question addValidValues(
+    String questionId,
+    Collection validValues) throws DMLException {
+    return null;
+  }
+
+  public int deleteQuestion(String questionId) throws DMLException {
+    return 0;
+  }
+
+  public int updateDisplayOrder(
+    String questionId,
+    int newDisplayOrder) throws DMLException {
+    return 0;
+  }
+
+  public int updateQuestionLongName(
+    String questionId,
+    String newLongName) throws DMLException {
+    return 0;
   }
 }
