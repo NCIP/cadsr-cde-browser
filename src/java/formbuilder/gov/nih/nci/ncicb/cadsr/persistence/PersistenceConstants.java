b5e861f434a860e163cb9ccf2586f6645f863de8
kakkodis <kakkodis>
2004-02-18 22:32:56 +0000

    Add all
    SVN-Revision: 99

--- src/java/formbuilder/gov/nih/nci/ncicb/cadsr/persistence/PersistenceConstants.java
+++ src/java/formbuilder/gov/nih/nci/ncicb/cadsr/persistence/PersistenceConstants.java
@@ -3,7 +3,6 @@ package gov.nih.nci.ncicb.cadsr.persistence;
 public interface PersistenceConstants {
   public static final String DAO_FACTORY_CLASS_KEY = "DAOFactoryClassName";
   public static final String DATASOURCE_LOCATION_KEY = "FormBuilderDS";
-  public static final String DATASOURCE_KEY = "DataSourceKey";
   public static final String DRIVER_MANAGER_DS = "DriverManagerDS";
   public static final String CONNECTION_STRING = "ConnectionString";
   public static final String DRIVER_CLASS_NAME = "DriverClassName";
