5d15a0fab768f6242ac2fe659fa0a76ad2d61f93
kakkodis <kakkodis>
2004-02-19 22:26:30 +0000

    Add all
    SVN-Revision: 114

--- src/java/formbuilder/gov/nih/nci/ncicb/cadsr/formbuilder/struts/formbeans/FormBuilderBaseDynaFormBean.java
+++ src/java/formbuilder/gov/nih/nci/ncicb/cadsr/formbuilder/struts/formbeans/FormBuilderBaseDynaFormBean.java
@@ -1,14 +1,23 @@
 package gov.nih.nci.ncicb.cadsr.formbuilder.struts.formbeans;
-import java.util.HashMap;
+
 import org.apache.struts.action.DynaActionForm;
 
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
+
+
 /**
- * Not decided what should go in here 
+ * Not decided what should go in here
  */
-public class FormBuilderBaseDynaFormBean extends DynaActionForm 
-{
-  public void clear()
-  {
-    dynaValues = new HashMap();
+public class FormBuilderBaseDynaFormBean extends DynaActionForm {
+  public void clear() {
+    Map map = getMap();
+    Iterator keys = map.keySet().iterator();
+
+    while (keys.hasNext()) {
+      String key = (String) keys.next();
+      map.put(key, "");
+    }
   }
 }
