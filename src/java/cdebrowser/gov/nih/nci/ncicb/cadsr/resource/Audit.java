5a6216da0062db6ac47c39658ccf4f652ab5834d
kakkodis <kakkodis>
2004-02-20 19:42:28 +0000

    Add all
    SVN-Revision: 119

--- src/java/cdebrowser/gov/nih/nci/ncicb/cadsr/resource/Audit.java
+++ src/java/cdebrowser/gov/nih/nci/ncicb/cadsr/resource/Audit.java
@@ -1,19 +1,22 @@
 package gov.nih.nci.ncicb.cadsr.resource;
 
+import gov.nih.nci.ncicb.cadsr.CaDSRConstants;
 import java.sql.Date;
 
-public interface Audit 
+public interface Audit extends CaDSRConstants
 {
   public String getCreatedBy();
-  public void setCreatedBy(String pCreatedBy); 
+  public void setCreatedBy(String pCreatedBy);
 
   public Date getDateCreated();
   public void setDateCreated(Date pCreatedDate);
 
   public String getModifiedBy();
-  public void setModifiedBy(String pModifiedBy); 
+  public void setModifiedBy(String pModifiedBy);
 
   public Date getDateModified();
   public void setDateModified(Date pModifiedDate);
+
+  public String toString();
 
 }
