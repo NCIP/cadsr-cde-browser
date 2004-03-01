c9b0a449041349e6b4e63848bd49002530e92951
kimh <kimh>
2004-03-01 20:25:47 +0000

    *** empty log message ***
    SVN-Revision: 160

--- src/java/formbuilder/gov/nih/nci/ncicb/cadsr/dto/FormValidValueInstructionTransferObject.java
+++ src/java/formbuilder/gov/nih/nci/ncicb/cadsr/dto/FormValidValueInstructionTransferObject.java
@@ -6,6 +6,9 @@ import gov.nih.nci.ncicb.cadsr.resource.FormValidValueInstruction;
 
 public class FormValidValueInstructionTransferObject
   extends InstructionTransferObject implements FormValidValueInstruction {
+
+  private FormValidValue formValidValue = null;
+  
   public FormValidValueInstructionTransferObject() {
   }
 
@@ -14,5 +17,28 @@ public class FormValidValueInstructionTransferObject
   }
 
   public void setFormValidValue(FormValidValue value) {
+    this.formValidValue = value;
   }
+
+  public String toString() {
+    StringBuffer sb = new StringBuffer();
+    sb.append(OBJ_SEPARATOR_START);
+    sb.append(super.toString());
+    sb.append(OBJ_SEPARATOR_END);
+    
+    FormValidValue formValidValue = getFormValidValue();
+
+    if (formValidValue != null) {
+      sb.append(ATTR_SEPARATOR + "FormValidValue=" + formValidValue);
+    }
+    else {
+      sb.append(ATTR_SEPARATOR + "FormValidValue=" + null);
+    }
+
+    sb.append(OBJ_SEPARATOR_END);
+
+    return sb.toString();
+  }
+  
+
 }
--- src/java/formbuilder/gov/nih/nci/ncicb/cadsr/dto/InstructionTransferObject.java
+++ src/java/formbuilder/gov/nih/nci/ncicb/cadsr/dto/InstructionTransferObject.java
@@ -5,20 +5,38 @@ import gov.nih.nci.ncicb.cadsr.resource.Instruction;
 
 public class InstructionTransferObject extends AdminComponentTransferObject
   implements Instruction {
+
+  private int displayOrder;
+  private String instrIdseq = null;
+  
   public InstructionTransferObject() {
   }
 
   public int getDisplayOrder() {
-    return 0;
+    return displayOrder;
   }
 
   public void setDisplayOrder(int order) {
+    this.displayOrder = order;
   }
 
   public String getInstrIdseq() {
-    return null;
+    return instrIdseq;
   }
 
   public void setInstrIdseq(String idseq) {
+    this.instrIdseq = idseq;
   }
+
+  public String toString() {
+    StringBuffer sb = new StringBuffer();
+    sb.append(OBJ_SEPARATOR_START);
+    sb.append(super.toString());
+    sb.append(ATTR_SEPARATOR + "displayOrder=" + getDisplayOrder());
+    sb.append(ATTR_SEPARATOR + "instrIdseq=" + getInstrIdseq());
+    sb.append(OBJ_SEPARATOR_END);
+
+    return sb.toString();
+  }
+  
 }
