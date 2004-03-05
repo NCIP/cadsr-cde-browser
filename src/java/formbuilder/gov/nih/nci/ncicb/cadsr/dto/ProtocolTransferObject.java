c470b9413fbd4bf0123cdcbd9d60a7cd001658a6
chilukur <chilukur>
2004-03-05 17:18:37 +0000

    *** empty log message ***
    SVN-Revision: 174

--- src/java/formbuilder/gov/nih/nci/ncicb/cadsr/dto/ProtocolTransferObject.java
+++ src/java/formbuilder/gov/nih/nci/ncicb/cadsr/dto/ProtocolTransferObject.java
@@ -1,9 +1,12 @@
 package gov.nih.nci.ncicb.cadsr.dto;
-import java.sql.Date;
+
 import gov.nih.nci.ncicb.cadsr.resource.Protocol;
 
-public class ProtocolTransferObject extends AdminComponentTransferObject implements Protocol 
-{
+import java.sql.Date;
+
+
+public class ProtocolTransferObject extends AdminComponentTransferObject
+  implements Protocol {
   String protocolIdseq = null;
   String longName = null;
   String leadOrg = null;
@@ -11,81 +14,67 @@ public class ProtocolTransferObject extends AdminComponentTransferObject impleme
   String protocolPhase = null;
   Date endDate = null;
   Date beginDate = null;
-  
-  
-  public ProtocolTransferObject(String longName)
-  {
-      setLongName(longName);
+
+  public ProtocolTransferObject(String longName) {
+    setLongName(longName);
   }
 
-  public String getProtoIdseq()
-  {
+  public String getProtoIdseq() {
     return protocolIdseq;
   }
 
-  public void setProtoIdseq(String protocolIdseq)
-  {
+  public void setProtoIdseq(String protocolIdseq) {
     this.protocolIdseq = protocolIdseq;
   }
 
-  public String getLeadOrg()
-  {
+  public String getLeadOrg() {
     return leadOrg;
   }
 
-  public void setLeadOrg(String leadOrg)
-  {
+  public void setLeadOrg(String leadOrg) {
     this.leadOrg = leadOrg;
   }
 
-  public String getType()
-  {
+  public String getType() {
     return protocolType;
   }
 
-  public void setType(String protocolType)
-  {
+  public void setType(String protocolType) {
     this.protocolType = protocolType;
   }
 
-  public String getPhase()
-  {
+  public String getPhase() {
     return protocolPhase;
   }
 
-  public void setPhase(String protocolPhase)
-  {
+  public void setPhase(String protocolPhase) {
     this.protocolPhase = protocolPhase;
   }
 
-  public Date getBeginDate()
-  {
+  public Date getBeginDate() {
     return beginDate;
   }
 
-  public void setBeginDate(Date beginDate)
-  {
+  public void setBeginDate(Date beginDate) {
     this.beginDate = beginDate;
   }
 
-  public Date getEndDate()
-  {
+  public Date getEndDate() {
     return endDate;
   }
 
-  public void setEndDate(Date endDate)
-  {
+  public void setEndDate(Date endDate) {
     this.endDate = endDate;
   }
-  
-  public String toString()
-  {
+
+  public String toString() {
     StringBuffer sb = new StringBuffer();
     sb.append(OBJ_SEPARATOR_START);
     sb.append(super.toString());
-    sb.append(ATTR_SEPARATOR+"longName="+getLongName());  
-    sb.append(ATTR_SEPARATOR+"protocolIdseq="+getProtoIdseq());
-    sb.append(OBJ_SEPARATOR_END);  
+    sb.append(ATTR_SEPARATOR + "longName=" + getLongName());
+    sb.append(ATTR_SEPARATOR + "protocolIdseq=" + getProtoIdseq());
+    sb.append(OBJ_SEPARATOR_END);
+
     return sb.toString();
   }
 }
