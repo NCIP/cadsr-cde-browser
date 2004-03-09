e299d0533691b5bba8dee3e8de6a9b4d94923427
aggarwap <aggarwap>
2004-03-09 19:28:37 +0000

    Added Insert and delete for cart item
    SVN-Revision: 181

--- src/java/formbuilder/gov/nih/nci/ncicb/cadsr/persistence/dao/jdbc/JDBCCDECartDAO.java
+++ src/java/formbuilder/gov/nih/nci/ncicb/cadsr/persistence/dao/jdbc/JDBCCDECartDAO.java
@@ -7,16 +7,20 @@ import gov.nih.nci.ncicb.cadsr.dto.CDECartItemTransferObject;
 import gov.nih.nci.ncicb.cadsr.dto.CDECartTransferObject;
 import gov.nih.nci.ncicb.cadsr.persistence.dao.CDECartDAO;
 import gov.nih.nci.ncicb.cadsr.servicelocator.ServiceLocator;
+import gov.nih.nci.ncicb.cadsr.servicelocator.SimpleServiceLocator;
 import gov.nih.nci.ncicb.cadsr.exception.DMLException;
 
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Types;
 
+import javax.sql.DataSource;
+
 import java.util.List;
 
 import org.springframework.jdbc.core.SqlParameter;
 import org.springframework.jdbc.object.MappingSqlQuery;
+import org.springframework.jdbc.object.SqlUpdate;
 
 
 public class JDBCCDECartDAO extends JDBCBaseDAO implements CDECartDAO {
@@ -38,12 +42,57 @@ public class JDBCCDECartDAO extends JDBCBaseDAO implements CDECartDAO {
     return cart;
   }
 
-  public int insertCartItem(CDECartItem item) throws DMLException {
-    return 0;
+  public int insertCartItem(CDECartItem item) throws DMLException {   // check if the user has the privilege to create module
+     InsertCartItem  cartItem  = 
+      new InsertCartItem (this.getDataSource());
+    String ccmIdseq = generateGUID(); 
+    int res = cartItem.createItem(item, ccmIdseq);
+    if (res != 1) {
+      throw new DMLException("Did not succeed creating item in the " + 
+        " cde_cart_items table.");
+    }
+    return 1;
   }
 
   public int deleteCartItem(String itemId) throws DMLException {
-    return 0;
+  DeleteCartItem  cartItem  = new DeleteCartItem (this.getDataSource());
+    int res = cartItem.deleteItem(itemId);
+    if (res != 1) {
+      throw new DMLException("Did not succeed in deleting  the " + 
+        " cde_cart_items table.");
+    }
+    return 1;
+}
+  public static void main(String[] args) {
+    ServiceLocator locator = new SimpleServiceLocator();
+
+    JDBCCDECartDAO cTest = new JDBCCDECartDAO(locator);
+
+    
+    try {
+      CDECartItem newItem = new CDECartItemTransferObject();
+
+      int res = cTest.deleteCartItem("D5378537-60EA-2B2C-E034-0003BA0B1A09");
+      System.out.println("\n*****Create Item Result 1: " + res);
+    }
+    catch (DMLException de) {
+      System.out.println("******Printing DMLException*******");
+      de.printStackTrace();
+      System.out.println("******Finishing printing DMLException*******");
+    }
+    
+    
+    /*
+    try {
+      int res = formTest.deleteForm("D4700045-2FD0-0DAA-E034-0003BA0B1A09");
+      System.out.println("\n*****Delete Form Result 1: " + res);
+    }
+    catch (DMLException de) {
+      System.out.println("******Printing DMLException*******");
+      de.printStackTrace();
+      System.out.println("******Finishing printing DMLException*******");
+    }
+    */
   }
 
   /**
@@ -85,6 +134,71 @@ public class JDBCCDECartDAO extends JDBCBaseDAO implements CDECartDAO {
       int rownum) throws SQLException {
       CDECartItem item = new CDECartItemTransferObject();
       return item;
+    }
+  }
+
+    /**
+   * Inner class that accesses database to create a cart item
+   */
+ private class InsertCartItem extends SqlUpdate {
+    public InsertCartItem(DataSource ds) {
+      String itemInsertSql = 
+      " INSERT INTO cde_cart_items " + 
+      " (ccm_idseq, ac_idseq, ua_name, actl_name,created_by,date_created) " +
+      " VALUES " +
+      " (?, ?, ?, ?, ?, ?) ";
+
+      this.setDataSource(ds);
+      this.setSql(itemInsertSql);
+      declareParameter(new SqlParameter("p_ccm_idseq", Types.VARCHAR));
+      declareParameter(new SqlParameter("p_ac_idseq", Types.VARCHAR));
+      declareParameter(new SqlParameter("p_ua_name", Types.VARCHAR));
+      declareParameter(new SqlParameter("p_actl_name", Types.VARCHAR));
+      declareParameter(new SqlParameter("p_created_by", Types.VARCHAR));
+      declareParameter(new SqlParameter("p_date_created", Types.TIMESTAMP));
+      compile();
+    }
+    protected int createItem (CDECartItem sm, String ccmIdseq) 
+    {
+      Object [] obj = 
+        new Object[]
+          {ccmIdseq, 
+           sm.getId(),
+           sm.getCreatedBy(),
+           sm.getType(),
+           sm.getCreatedBy(),
+           sm.getCreatedDate()
+          };
+      
+	    int res = update(obj);
+      return res;
+    }
+  }
+
+  /**
+   * Inner class that accesses database to delete an item.
+   */
+ private class DeleteCartItem extends SqlUpdate {
+    public DeleteCartItem(DataSource ds) {
+      String itemDeleteSql = 
+      " DELETE FROM cde_cart_items " +
+      " WHERE ccm_idseq =  " +
+      " (?) ";
+
+      this.setDataSource(ds);
+      this.setSql(itemDeleteSql);
+      declareParameter(new SqlParameter("p_ccm_idseq", Types.VARCHAR));
+      compile();
+    }
+    protected int deleteItem (String ccmIdseq) 
+    {
+      Object [] obj = 
+        new Object[]
+          {ccmIdseq
+          };
+      
+	    int res = update(obj);
+      return res;
     }
   }
 }
