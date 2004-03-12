492a879dc9aa704d21b166920fe49ae4db6688b4
ludetc <ludetc>
2004-03-12 21:11:25 +0000

    Added checking for not null in setFormforAction
    SVN-Revision: 219

--- src/java/formbuilder/gov/nih/nci/ncicb/cadsr/formbuilder/struts/actions/FormBuilderBaseDispatchAction.java
+++ src/java/formbuilder/gov/nih/nci/ncicb/cadsr/formbuilder/struts/actions/FormBuilderBaseDispatchAction.java
@@ -39,197 +39,199 @@ import javax.servlet.http.HttpSession;
  * Base DispatchAction for all formbuilder DispatchActions
  */
 public class FormBuilderBaseDispatchAction extends DispatchAction
-  implements FormConstants, NavigationConstants,
-   PersistenceConstants,FormBuilderConstants,
-    CaDSRConstants {
-  protected static Log log = LogFactory.getLog(FormAction.class.getName());
+    implements FormConstants, NavigationConstants,
+	       PersistenceConstants,FormBuilderConstants,
+	       CaDSRConstants {
+    protected static Log log = LogFactory.getLog(FormAction.class.getName());
 
-  /**
-   * Retrieve an object from the application scope by its name. This is a
-   * convience method.
-   */
-  protected Object getApplicationObject(String attrName) {
-    return servlet.getServletContext().getAttribute(attrName);
-  }
-
-  /**
-   * Retrieve a session object based on the request and the attribute name.
-   */
-  protected Object getSessionObject(
-    HttpServletRequest req,
-    String attrName) {
-    Object sessionObj = null;
-    HttpSession session = req.getSession(false);
-
-    if (session != null) {
-      sessionObj = session.getAttribute(attrName);
+    /**
+     * Retrieve an object from the application scope by its name. This is a
+     * convience method.
+     */
+    protected Object getApplicationObject(String attrName) {
+	return servlet.getServletContext().getAttribute(attrName);
     }
 
-    return sessionObj;
-  }
+    /**
+     * Retrieve a session object based on the request and the attribute name.
+     */
+    protected Object getSessionObject(
+				      HttpServletRequest req,
+				      String attrName) {
+	Object sessionObj = null;
+	HttpSession session = req.getSession(false);
 
-  /**
-   * Add an object to session based on the request and the attribute name.
-   */
-  protected void setSessionObject(
-    HttpServletRequest req,
-    String attrName,
-    Object sessionObject) {
-    HttpSession session = req.getSession(false);
+	if (session != null) {
+	    sessionObj = session.getAttribute(attrName);
+	}
 
-    if (session != null) {
-      session.setAttribute(attrName, sessionObject);
-    }
-  }
-
-  /**
-   * Gets the ServiceDelegateFactory form the application scope and
-   * instantiates a FormBuilderServiceDelegate from the factory
-   *
-   * @return FormBuilderServiceDelegate
-   *
-   * @throws ServiceStartupException
-   */
-  protected FormBuilderServiceDelegate getFormBuilderService()
-    throws ServiceStartupException {
-    FormBuilderServiceDelegate svcDelegate = null;
-    ServiceDelegateFactory svcFactory =
-      (ServiceDelegateFactory) getApplicationObject(
-        FormBuilderConstants.SERVICE_DELEGATE_FACTORY_KEY);
-    svcDelegate = svcFactory.createService();
-
-    return svcDelegate;
-  }
-
-  /**
-   * Initializes the lookupvalues(contexts,categories,workflows into session)
-   *
-   * @return ActionForward
-   *
-   * @throws Exception
-   */
-  protected void setInitLookupValues(HttpServletRequest req) {
-    Object obj = getSessionObject(req, ALL_CONTEXTS);
-
-    if (obj == null) {
-      Collection contexts = getFormBuilderService().getAllContexts();
-      setSessionObject(req, ALL_CONTEXTS, contexts);
+	return sessionObj;
     }
 
-    obj = getSessionObject(req, ALL_WORKFLOWS);
+    /**
+     * Add an object to session based on the request and the attribute name.
+     */
+    protected void setSessionObject(
+				    HttpServletRequest req,
+				    String attrName,
+				    Object sessionObject) {
+	HttpSession session = req.getSession(false);
 
-    if (obj == null) {
-      Collection workflows =
-        getFormBuilderService().getStatusesForACType(FORM_ADMIN_COMPONENT_TYPE);
-      setSessionObject(req, ALL_WORKFLOWS, workflows);
+	if (session != null) {
+	    session.setAttribute(attrName, sessionObject);
+	}
     }
 
-    obj = getSessionObject(req, ALL_FORM_CATEGORIES);
+    /**
+     * Gets the ServiceDelegateFactory form the application scope and
+     * instantiates a FormBuilderServiceDelegate from the factory
+     *
+     * @return FormBuilderServiceDelegate
+     *
+     * @throws ServiceStartupException
+     */
+    protected FormBuilderServiceDelegate getFormBuilderService()
+	throws ServiceStartupException {
+	FormBuilderServiceDelegate svcDelegate = null;
+	ServiceDelegateFactory svcFactory =
+	    (ServiceDelegateFactory) getApplicationObject(
+							  FormBuilderConstants.SERVICE_DELEGATE_FACTORY_KEY);
+	svcDelegate = svcFactory.createService();
 
-    if (obj == null) {
-      Collection categories = getFormBuilderService().getAllFormCategories();
-      setSessionObject(req, ALL_FORM_CATEGORIES, categories);
+	return svcDelegate;
     }
 
-    obj = getSessionObject(req, ALL_FORM_TYPES);
+    /**
+     * Initializes the lookupvalues(contexts,categories,workflows into session)
+     *
+     * @return ActionForward
+     *
+     * @throws Exception
+     */
+    protected void setInitLookupValues(HttpServletRequest req) {
+	Object obj = getSessionObject(req, ALL_CONTEXTS);
 
-    if (obj == null) {
-      Collection types = Arrays.asList(FORM_TYPE_VALUES);
-      setSessionObject(req, ALL_FORM_TYPES, types);
-    }
-  }
+	if (obj == null) {
+	    Collection contexts = getFormBuilderService().getAllContexts();
+	    setSessionObject(req, ALL_CONTEXTS, contexts);
+	}
 
-  /**
-   * If a iconForm(DynaForm) exist then get the FormDetails for the formIdSeq
-   * is retrived.
-   *
-   * @param form The optional ActionForm bean for this request.
-   * @param request The HTTP Request we are processing.
-   */
-  protected Form setFormForAction(
-    ActionForm form,
-    HttpServletRequest request) throws FormBuilderException {
-    FormBuilderServiceDelegate service = getFormBuilderService();
-    DynaActionForm hrefCRFForm = (DynaActionForm) form;
-    Form crf = null;
+	obj = getSessionObject(req, ALL_WORKFLOWS);
 
-    if (hrefCRFForm != null) {
-      String formIdSeq = (String) hrefCRFForm.get(FORM_ID_SEQ);
-      crf = service.getFormDetails(formIdSeq);
-      setSessionObject(request, CRF, crf);
-    }
-    else {
-      setSessionObject(request, CRF, null);
-    }
+	if (obj == null) {
+	    Collection workflows =
+		getFormBuilderService().getStatusesForACType(FORM_ADMIN_COMPONENT_TYPE);
+	    setSessionObject(req, ALL_WORKFLOWS, workflows);
+	}
 
-    return crf;
-  }
+	obj = getSessionObject(req, ALL_FORM_CATEGORIES);
 
-  protected void saveMessage(
-    String key,
-    HttpServletRequest request) {
-    if (key != null) {
-      ActionMessage errorMessage = new ActionMessage(key);
-      ActionMessages messages = new ActionMessages();
-      messages.add(messages.GLOBAL_MESSAGE, errorMessage);
-      saveMessages(request, messages);
-    }
-  }
+	if (obj == null) {
+	    Collection categories = getFormBuilderService().getAllFormCategories();
+	    setSessionObject(req, ALL_FORM_CATEGORIES, categories);
+	}
 
-  /**
-   * This Action forwards to the default formbuilder home.
-   *
-   * @param mapping The ActionMapping used to select this instance.
-   * @param form The optional ActionForm bean for this request.
-   * @param request The HTTP Request we are processing.
-   * @param response The HTTP Response we are processing.
-   *
-   * @return
-   *
-   * @throws IOException
-   * @throws ServletException
-   */
-  public ActionForward sendHome(
-    ActionMapping mapping,
-    ActionForm form,
-    HttpServletRequest request,
-    HttpServletResponse response) throws IOException, ServletException {
-    setInitLookupValues(request);
+	obj = getSessionObject(req, ALL_FORM_TYPES);
 
-    return mapping.findForward(DEFAULT_HOME);
-  }
-
-
-  /**
-   * Sets default method name if no method is specified
-   *
-   * @return ActionForward
-   *
-   * @throws Exception
-   */
-  protected ActionForward dispatchMethod(
-    ActionMapping mapping,
-    ActionForm form,
-    HttpServletRequest request,
-    HttpServletResponse response,
-    String name) throws Exception {
-    if ((name == null) || name.equals("")) {
-      name = DEFAULT_METHOD;
+	if (obj == null) {
+	    Collection types = Arrays.asList(FORM_TYPE_VALUES);
+	    setSessionObject(req, ALL_FORM_TYPES, types);
+	}
     }
 
-    try {
-      return super.dispatchMethod(mapping, form, request, response, name);
+    /**
+     * If a iconForm(DynaForm) exist then get the FormDetails for the formIdSeq
+     * is retrived.
+     *
+     * @param form The optional ActionForm bean for this request.
+     * @param request The HTTP Request we are processing.
+     */
+    protected Form setFormForAction(
+				    ActionForm form,
+				    HttpServletRequest request) throws FormBuilderException {
+	FormBuilderServiceDelegate service = getFormBuilderService();
+	DynaActionForm hrefCRFForm = (DynaActionForm) form;
+	Form crf = null;
+
+	if (hrefCRFForm != null) {
+	    String formIdSeq = (String) hrefCRFForm.get(FORM_ID_SEQ);
+	    if ((formIdSeq != null) && (formIdSeq.length() > 0)) {
+		crf = service.getFormDetails(formIdSeq);
+		setSessionObject(request, CRF, crf);
+	    } 
+	}
+	else {
+	    setSessionObject(request, CRF, null);
+	}
+
+	return crf;
     }
-    catch (Throwable throwable) {
-      if (log.isFatalEnabled()) {
-        NCIUser user = (NCIUser) getSessionObject(request, USER_KEY);
-        if(user!=null)
-          log.fatal(user.getUsername(), throwable);
-        else
-          log.fatal( throwable);
-      }     
-      saveMessage(ERROR_FATAL, request);
-      throw new FatalException(throwable);
+
+    protected void saveMessage(
+			       String key,
+			       HttpServletRequest request) {
+	if (key != null) {
+	    ActionMessage errorMessage = new ActionMessage(key);
+	    ActionMessages messages = new ActionMessages();
+	    messages.add(messages.GLOBAL_MESSAGE, errorMessage);
+	    saveMessages(request, messages);
+	}
     }
-  }
+
+    /**
+     * This Action forwards to the default formbuilder home.
+     *
+     * @param mapping The ActionMapping used to select this instance.
+     * @param form The optional ActionForm bean for this request.
+     * @param request The HTTP Request we are processing.
+     * @param response The HTTP Response we are processing.
+     *
+     * @return
+     *
+     * @throws IOException
+     * @throws ServletException
+     */
+    public ActionForward sendHome(
+				  ActionMapping mapping,
+				  ActionForm form,
+				  HttpServletRequest request,
+				  HttpServletResponse response) throws IOException, ServletException {
+	setInitLookupValues(request);
+
+	return mapping.findForward(DEFAULT_HOME);
+    }
+
+
+    /**
+     * Sets default method name if no method is specified
+     *
+     * @return ActionForward
+     *
+     * @throws Exception
+     */
+    protected ActionForward dispatchMethod(
+					   ActionMapping mapping,
+					   ActionForm form,
+					   HttpServletRequest request,
+					   HttpServletResponse response,
+					   String name) throws Exception {
+	if ((name == null) || name.equals("")) {
+	    name = DEFAULT_METHOD;
+	}
+
+	try {
+	    return super.dispatchMethod(mapping, form, request, response, name);
+	}
+	catch (Throwable throwable) {
+	    if (log.isFatalEnabled()) {
+		NCIUser user = (NCIUser) getSessionObject(request, USER_KEY);
+		if(user!=null)
+		    log.fatal(user.getUsername(), throwable);
+		else
+		    log.fatal( throwable);
+	    }     
+	    saveMessage(ERROR_FATAL, request);
+	    throw new FatalException(throwable);
+	}
+    }
 }
