72b3735d0b5d1e0f93879dd2bd798e39f81bc9b4
ludetc <ludetc>
2004-03-30 16:15:51 +0000

    Used by cancel buttons, as simple controller --> forward methods
    SVN-Revision: 356

---
+++ src/java/formbuilder/gov/nih/nci/ncicb/cadsr/formbuilder/struts/actions/CancelAction.java
@@ -0,0 +1,57 @@
+package gov.nih.nci.ncicb.cadsr.formbuilder.struts.actions;
+
+import gov.nih.nci.ncicb.cadsr.formbuilder.common.FormBuilderException;
+import gov.nih.nci.ncicb.cadsr.formbuilder.service.FormBuilderServiceDelegate;
+import gov.nih.nci.ncicb.cadsr.formbuilder.struts.formbeans.FormBuilderBaseDynaFormBean;
+import gov.nih.nci.ncicb.cadsr.jsp.bean.PaginationBean;
+import gov.nih.nci.ncicb.cadsr.resource.Form;
+import gov.nih.nci.ncicb.cadsr.util.StringUtils;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+
+import org.apache.struts.Globals;
+import org.apache.struts.action.ActionForm;
+import org.apache.struts.action.ActionForward;
+import org.apache.struts.action.ActionMapping;
+import org.apache.struts.action.ActionMessage;
+import org.apache.struts.action.ActionMessages;
+import org.apache.struts.action.DynaActionForm;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import gov.nih.nci.ncicb.cadsr.dto.FormTransferObject;
+import gov.nih.nci.ncicb.cadsr.dto.ContextTransferObject;
+import gov.nih.nci.ncicb.cadsr.dto.ProtocolTransferObject;
+import gov.nih.nci.ncicb.cadsr.resource.Context;
+import gov.nih.nci.ncicb.cadsr.resource.Protocol;
+
+
+public class CancelAction extends FormBuilderBaseDispatchAction {
+
+    public ActionForward getModuleToEdit(
+				       ActionMapping mapping,
+				       ActionForm form,
+				       HttpServletRequest request,
+				       HttpServletResponse response) throws IOException, ServletException {
+
+
+	return mapping.findForward("gotoModuleEdit");
+    }
+
+    public ActionForward getFormToEdit(
+				       ActionMapping mapping,
+				       ActionForm form,
+				       HttpServletRequest request,
+				       HttpServletResponse response) throws IOException, ServletException {
+
+
+	return mapping.findForward("gotoFormEdit");
+    }
+
+
+}
+  
