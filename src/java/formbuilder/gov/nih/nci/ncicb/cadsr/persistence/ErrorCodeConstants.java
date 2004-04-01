e1a68edf40d57325ae6b66fab6136bb40d76c2b1
kakkodis <kakkodis>
2004-04-01 17:20:43 +0000

    [no comments]
    SVN-Revision: 375

--- src/java/formbuilder/gov/nih/nci/ncicb/cadsr/persistence/ErrorCodeConstants.java
+++ src/java/formbuilder/gov/nih/nci/ncicb/cadsr/persistence/ErrorCodeConstants.java
@@ -6,5 +6,9 @@ public interface ErrorCodeConstants
  public static final String ERROR_MODULE_TO_DELETE_DOES_NOT_EXIST = "DML002";
  public static final String ERROR_MODULE_TO_UPDATE_DOES_NOT_EXIST = "DML003";
  public static final String ERROR_DELETE_FORM_FAILED = "DML004";
-
+ public static final String INSUFFICIENT_PRIVILEGES = "DML005";
+ public static final String ERROR_CREATEING_VALID_VALUE = "DML006";
+ public static final String ERROR_DELETEING_VALID_VALUE = "DML007";
+ public static final String ERROR_UPDATING_VALID_VALUE = "DML008";
+ public static final String ERROR_UPDATING_DISPLAY_ORDER = "DML009";
 }
