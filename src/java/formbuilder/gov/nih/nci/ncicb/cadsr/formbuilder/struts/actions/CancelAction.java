1b91fe1592ce29f44c48156d6d63c27463979fee
ludetc <ludetc>
2004-04-02 16:12:36 +0000

    Added manage classifications methods Added Cancel Action for cancel buttons
    SVN-Revision: 393

--- src/java/formbuilder/gov/nih/nci/ncicb/cadsr/formbuilder/struts/actions/CancelAction.java
+++ src/java/formbuilder/gov/nih/nci/ncicb/cadsr/formbuilder/struts/actions/CancelAction.java
@@ -1,10 +1,15 @@
 package gov.nih.nci.ncicb.cadsr.formbuilder.struts.actions;
 
+import gov.nih.nci.ncicb.cadsr.dto.ContextTransferObject;
+import gov.nih.nci.ncicb.cadsr.dto.FormTransferObject;
+import gov.nih.nci.ncicb.cadsr.dto.ProtocolTransferObject;
 import gov.nih.nci.ncicb.cadsr.formbuilder.common.FormBuilderException;
 import gov.nih.nci.ncicb.cadsr.formbuilder.service.FormBuilderServiceDelegate;
 import gov.nih.nci.ncicb.cadsr.formbuilder.struts.formbeans.FormBuilderBaseDynaFormBean;
 import gov.nih.nci.ncicb.cadsr.jsp.bean.PaginationBean;
+import gov.nih.nci.ncicb.cadsr.resource.Context;
 import gov.nih.nci.ncicb.cadsr.resource.Form;
+import gov.nih.nci.ncicb.cadsr.resource.Protocol;
 import gov.nih.nci.ncicb.cadsr.util.StringUtils;
 
 import org.apache.commons.logging.Log;
@@ -23,35 +28,27 @@ import java.io.IOException;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import gov.nih.nci.ncicb.cadsr.dto.FormTransferObject;
-import gov.nih.nci.ncicb.cadsr.dto.ContextTransferObject;
-import gov.nih.nci.ncicb.cadsr.dto.ProtocolTransferObject;
-import gov.nih.nci.ncicb.cadsr.resource.Context;
-import gov.nih.nci.ncicb.cadsr.resource.Protocol;
 
 
 public class CancelAction extends FormBuilderBaseDispatchAction {
-
-    public ActionForward getModuleToEdit(
-				       ActionMapping mapping,
-				       ActionForm form,
-				       HttpServletRequest request,
-				       HttpServletResponse response) throws IOException, ServletException {
-
-
-	return mapping.findForward("gotoModuleEdit");
+    public ActionForward getModuleToEdit(ActionMapping mapping,
+        ActionForm form, HttpServletRequest request,
+        HttpServletResponse response) throws IOException, ServletException {
+        return mapping.findForward("gotoModuleEdit");
     }
 
-    public ActionForward getFormToEdit(
-				       ActionMapping mapping,
-				       ActionForm form,
-				       HttpServletRequest request,
-				       HttpServletResponse response) throws IOException, ServletException {
-
-
-	return mapping.findForward("gotoFormEdit");
+    public ActionForward getFormToEdit(ActionMapping mapping, ActionForm form,
+        HttpServletRequest request, HttpServletResponse response)
+        throws IOException, ServletException {
+        return mapping.findForward("gotoFormEdit");
     }
 
+    public ActionForward gotoSearch(ActionMapping mapping, ActionForm form,
+        HttpServletRequest request, HttpServletResponse response)
+        throws IOException, ServletException {
 
+//         ((DynaActionForm]) form).set(METHOD_PARAM, GET_ALL_FORMS_METHOD);
+
+        return mapping.findForward("gotoSearch");
+    }
 }
-  
