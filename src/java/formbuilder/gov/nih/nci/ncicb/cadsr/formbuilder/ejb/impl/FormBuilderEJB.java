ccb50ca9ad4a2eb98c983fe12afd75155fc446cf
kakkodis <kakkodis>
2004-04-05 17:31:48 +0000

    Add all
    SVN-Revision: 424

--- src/java/formbuilder/gov/nih/nci/ncicb/cadsr/formbuilder/ejb/impl/FormBuilderEJB.java
+++ src/java/formbuilder/gov/nih/nci/ncicb/cadsr/formbuilder/ejb/impl/FormBuilderEJB.java
@@ -120,166 +120,171 @@ public class FormBuilderEJB extends SessionBeanAdapter
         return myForm;
     }
 
-    public Module getModule(String modulePK) {
-        ModuleDAO mdao = daoFactory.getModuleDAO();
-        QuestionDAO qdao = daoFactory.getQuestionDAO();
-        FormValidValueDAO vdao = daoFactory.getFormValidValueDAO();
+  public Module getModule(String modulePK) {
 
-        List questions = (List) mdao.getQuestionsInAModule(modulePK);
-        Iterator qIter = questions.iterator();
-        Question term = null;
-        Module module = null;
+    ModuleDAO mdao = daoFactory.getModuleDAO();
+    QuestionDAO qdao = daoFactory.getQuestionDAO();
+    FormValidValueDAO vdao = daoFactory.getFormValidValueDAO();
 
-        while (qIter.hasNext()) {
-            term = (Question) qIter.next();
+    Module module = mdao.findModuleByPrimaryKey(modulePK);
+    List questions = (List) mdao.getQuestionsInAModule(modulePK);
+    Iterator qIter = questions.iterator();
+    Question term =null;
 
-            String termId = term.getQuesIdseq();
-            List values = (List) qdao.getValidValues(termId);
-            term.setValidValues(values);
-        }
+    while (qIter.hasNext()) {
+        term = (Question) qIter.next();
+        String termId = term.getQuesIdseq();
+        List values = (List) qdao.getValidValues(termId);
+        term.setValidValues(values);
+      }
 
-        module.setQuestions(questions);
+    module.setQuestions(questions);
 
-        return module;
+    return module;
+  }
+
+  public Module updateModule(
+       String moduleIdSeq,Module moduleHeader,
+       Collection updatedQuestions,
+       Collection deletedQuestions,
+       Collection newQuestions,
+       Map updatedValidValues,
+       Map addedValidValues,
+       Map deletedValidValues)
+       {
+         ModuleDAO moduleDao = daoFactory.getModuleDAO();
+         QuestionDAO questionDao = daoFactory.getQuestionDAO();
+         FormValidValueDAO formValidValueDao = daoFactory.getFormValidValueDAO();
+
+         if(moduleHeader!=null)
+         {
+           moduleHeader.setModifiedBy(getUserName());
+           moduleDao.updateModuleLongName(moduleHeader.getModuleIdseq(),moduleHeader.getLongName());
+         }
+
+         if(updatedQuestions!=null&&!updatedQuestions.isEmpty())
+         {
+           Iterator updatedIt = updatedQuestions.iterator();
+           while(updatedIt.hasNext())
+           {
+             Question currQuestion = (Question)updatedIt.next();
+             currQuestion.setModifiedBy(getUserName());
+             questionDao.updateQuestionLongNameDispOrderDeIdseq(currQuestion);
+           }
+         }
+         if(deletedQuestions!=null&&!deletedQuestions.isEmpty())
+         {
+           Iterator deletedIt = deletedQuestions.iterator();
+           while(deletedIt.hasNext())
+           {
+             Question currQuestion = (Question)deletedIt.next();
+             questionDao.deleteQuestion(currQuestion.getQuesIdseq());
+           }
+         }
+         if(newQuestions!=null&&!newQuestions.isEmpty())
+         {
+           Iterator newIt = newQuestions.iterator();
+           while(newIt.hasNext())
+           {
+             Question currQuestion = (Question)newIt.next();
+             currQuestion.setCreatedBy(getUserName());
+             questionDao.createQuestionComponent(currQuestion);
+             List currQuestionValidValues = currQuestion.getValidValues();
+             ListIterator currQuestionValidValuesIt = currQuestionValidValues.listIterator();
+             while(currQuestionValidValuesIt!=null&&currQuestionValidValuesIt.hasNext())
+             {
+               FormValidValue fvv = (FormValidValue)currQuestionValidValuesIt.next();
+               formValidValueDao.createFormValidValueComponent(fvv);
+             }
+           }
+         }
+         if(updatedValidValues!=null&&!updatedValidValues.isEmpty())
+         {
+           Set keySet = updatedValidValues.keySet();
+           Iterator keyIt = keySet.iterator();
+           while(keyIt.hasNext())
+           {
+             String questionIdSeq = (String)keyIt.next();
+             List validValueList = (List)updatedValidValues.get(questionIdSeq);
+             ListIterator vvListIt = validValueList.listIterator();
+             while(vvListIt.hasNext())
+             {
+               FormValidValue fvv = (FormValidValue)vvListIt.next();
+               fvv.setModifiedBy(getUserName());
+               formValidValueDao.updateDisplayOrder(fvv.getValueIdseq(),fvv.getDisplayOrder());
+             }
+           }
+         }
+         if(addedValidValues!=null&&!addedValidValues.isEmpty())
+         {
+           Set keySet = addedValidValues.keySet();
+           Iterator keyIt = keySet.iterator();
+           while(keyIt.hasNext())
+           {
+             String questionIdSeq = (String)keyIt.next();
+             List validValueList = (List)addedValidValues.get(questionIdSeq);
+             ListIterator vvListIt = validValueList.listIterator();
+             while(vvListIt.hasNext())
+             {
+               FormValidValue fvv = (FormValidValue)vvListIt.next();
+               fvv.setCreatedBy(getUserName());
+               formValidValueDao.createFormValidValueComponent(fvv);
+             }
+           }
+         }
+         if(deletedValidValues!=null&&!deletedValidValues.isEmpty())
+         {
+           Set keySet = deletedValidValues.keySet();
+           Iterator keyIt = keySet.iterator();
+           while(keyIt.hasNext())
+           {
+             String questionIdSeq = (String)keyIt.next();
+             List validValueList = (List)deletedValidValues.get(questionIdSeq);
+             ListIterator vvListIt = validValueList.listIterator();
+             while(vvListIt.hasNext())
+             {
+               FormValidValue fvv = (FormValidValue)vvListIt.next();
+               formValidValueDao.deleteFormValidValue(fvv.getValueIdseq());
+             }
+           }
+         }
+
+         return getModule(moduleIdSeq);
+       }
+
+  public Form updateForm(
+    Form formHeader,
+    Collection updatedModules,
+    Collection deletedModules) {
+    ModuleDAO dao = daoFactory.getModuleDAO();
+    FormDAO formdao = daoFactory.getFormDAO();
+   if(formHeader!=null)
+   {
+     formdao.updateFormComponent(formHeader);
+   }
+    if ((updatedModules != null) && !updatedModules.isEmpty()) {
+      Iterator updatedIt = updatedModules.iterator();
+
+      while (updatedIt.hasNext()) {
+        Module updatedModule = (Module) updatedIt.next();
+        dao.updateDisplayOrder(
+          updatedModule.getModuleIdseq(), updatedModule.getDisplayOrder());
+      }
     }
 
-    public Module updateModule(String moduleIdSeq, Module moduleHeader,
-        Collection updatedQuestions, Collection deletedQuestions,
-        Collection newQuestions, Map updatedValidValues, Map addedValidValues,
-        Map deletedValidValues) {
-        ModuleDAO moduleDao = daoFactory.getModuleDAO();
-        QuestionDAO questionDao = daoFactory.getQuestionDAO();
-        FormValidValueDAO formValidValueDao = daoFactory.getFormValidValueDAO();
+    //Delete Modules
+    if ((deletedModules != null) && !deletedModules.isEmpty()) {
+      Iterator deletedIt = deletedModules.iterator();
 
-        if (moduleHeader != null) {
-            moduleHeader.setModifiedBy(getUserName());
-            moduleDao.updateModuleLongName(moduleHeader.getModuleIdseq(),
-                moduleHeader.getLongName());
-        }
-
-        if ((updatedQuestions != null) && !updatedQuestions.isEmpty()) {
-            Iterator updatedIt = updatedQuestions.iterator();
-
-            while (updatedIt.hasNext()) {
-                Question currQuestion = (Question) updatedIt.next();
-                currQuestion.setModifiedBy(getUserName());
-                questionDao.updateQuestionLongNameDispOrderDeIdseq(currQuestion);
-            }
-        }
-
-        if ((deletedQuestions != null) && !deletedQuestions.isEmpty()) {
-            Iterator deletedIt = deletedQuestions.iterator();
-
-            while (deletedIt.hasNext()) {
-                Question currQuestion = (Question) deletedIt.next();
-                questionDao.deleteQuestion(currQuestion.getQuesIdseq());
-            }
-        }
-
-        if ((newQuestions != null) && !newQuestions.isEmpty()) {
-            Iterator newIt = newQuestions.iterator();
-
-            while (newIt.hasNext()) {
-                Question currQuestion = (Question) newIt.next();
-                currQuestion.setCreatedBy(getUserName());
-                questionDao.createQuestionComponent(currQuestion);
-
-                List currQuestionValidValues = currQuestion.getValidValues();
-                ListIterator currQuestionValidValuesIt = currQuestionValidValues.listIterator();
-
-                while ((currQuestionValidValuesIt != null) &&
-                        currQuestionValidValuesIt.hasNext()) {
-                    FormValidValue fvv = (FormValidValue) currQuestionValidValuesIt.next();
-                    formValidValueDao.createFormValidValueComponent(fvv);
-                }
-            }
-        }
-
-        if ((updatedValidValues != null) && !updatedValidValues.isEmpty()) {
-            Set keySet = updatedValidValues.keySet();
-            Iterator keyIt = keySet.iterator();
-
-            while (keyIt.hasNext()) {
-                String questionIdSeq = (String) keyIt.next();
-                List validValueList = (List) updatedValidValues.get(questionIdSeq);
-                ListIterator vvListIt = validValueList.listIterator();
-
-                while (vvListIt.hasNext()) {
-                    FormValidValue fvv = (FormValidValue) vvListIt.next();
-                    fvv.setModifiedBy(getUserName());
-                    formValidValueDao.updateDisplayOrder(fvv.getValueIdseq(),
-                        fvv.getDisplayOrder());
-                }
-            }
-        }
-
-        if ((addedValidValues != null) && !addedValidValues.isEmpty()) {
-            Set keySet = addedValidValues.keySet();
-            Iterator keyIt = keySet.iterator();
-
-            while (keyIt.hasNext()) {
-                String questionIdSeq = (String) keyIt.next();
-                List validValueList = (List) addedValidValues.get(questionIdSeq);
-                ListIterator vvListIt = validValueList.listIterator();
-
-                while (vvListIt.hasNext()) {
-                    FormValidValue fvv = (FormValidValue) vvListIt.next();
-                    fvv.setCreatedBy(getUserName());
-                    formValidValueDao.createFormValidValueComponent(fvv);
-                }
-            }
-        }
-
-        if ((deletedValidValues != null) && !deletedValidValues.isEmpty()) {
-            Set keySet = deletedValidValues.keySet();
-            Iterator keyIt = keySet.iterator();
-
-            while (keyIt.hasNext()) {
-                String questionIdSeq = (String) keyIt.next();
-                List validValueList = (List) deletedValidValues.get(questionIdSeq);
-                ListIterator vvListIt = validValueList.listIterator();
-
-                while (vvListIt.hasNext()) {
-                    FormValidValue fvv = (FormValidValue) vvListIt.next();
-                    formValidValueDao.deleteFormValidValue(fvv.getValueIdseq());
-                }
-            }
-        }
-
-        return getModule(moduleHeader.getModuleIdseq());
+      while (deletedIt.hasNext()) {
+        Module deletedModule = (Module) deletedIt.next();
+        dao.deleteModule(deletedModule.getModuleIdseq());
+      }
     }
 
-    public Form updateForm(Form formHeader, Collection updatedModules,
-        Collection deletedModules) {
-        ModuleDAO dao = daoFactory.getModuleDAO();
-        FormDAO formdao = daoFactory.getFormDAO();
+    return getFormDetails(formHeader.getFormIdseq());
+  }
 
-        if (formHeader != null) {
-            formdao.updateFormComponent(formHeader);
-        }
-
-        if ((updatedModules != null) && !updatedModules.isEmpty()) {
-            Iterator updatedIt = updatedModules.iterator();
-
-            while (updatedIt.hasNext()) {
-                Module updatedModule = (Module) updatedIt.next();
-                dao.updateDisplayOrder(updatedModule.getModuleIdseq(),
-                    updatedModule.getDisplayOrder());
-            }
-        }
-
-        //Delete Modules
-        if ((deletedModules != null) && !deletedModules.isEmpty()) {
-            Iterator deletedIt = deletedModules.iterator();
-
-            while (deletedIt.hasNext()) {
-                Module deletedModule = (Module) deletedIt.next();
-                dao.deleteModule(deletedModule.getModuleIdseq());
-            }
-        }
-
-        return getFormDetails(formHeader.getFormIdseq());
-    }
 
     public Form getFormRow(String formPK) {
         FormDAO dao = daoFactory.getFormDAO();
@@ -480,7 +485,7 @@ public class FormBuilderEJB extends SessionBeanAdapter
           formHeaderInstruction.setForm(newForm);
           fidao1.createFormInstructionComponent(formHeaderInstruction);
         }
-        
+
         if (formHeaderInstruction != null)
         {
           FormInstructionDAO fidao2 = daoFactory.getFormInstructionDAO();
