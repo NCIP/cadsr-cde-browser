3b66613c49e3dd42b98bea1641ecb29cac53365b
kakkodis <kakkodis>
2004-04-19 18:36:51 +0000

    Add all
    SVN-Revision: 570

---
+++ src/java/common/gov/nih/nci/ncicb/cadsr/util/SessionUtils.java
@@ -0,0 +1,29 @@
+package gov.nih.nci.ncicb.cadsr.util;
+
+import gov.nih.nci.ncicb.cadsr.CaDSRConstants;
+import java.util.ArrayList;
+import java.util.Collection;
+
+import javax.servlet.http.HttpSession;
+
+
+public class SessionUtils {
+  public SessionUtils() {
+  }
+
+  public static void addGlobalSessionKey(
+    HttpSession session,
+    String key) {
+    if (session != null) {
+      Collection keys =
+        (Collection) session.getAttribute(CaDSRConstants.GLOBAL_SESSION_KEYS);
+
+      if (keys == null) {
+        keys = new ArrayList();
+      }
+
+      keys.add(key);
+      session.setAttribute(CaDSRConstants.GLOBAL_SESSION_KEYS, keys);
+    }
+  }
+}
