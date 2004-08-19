c8e0bbaf5987345bbaa217f7a9c58cb1beec5b24
jiangja <jiangja>
2004-08-19 19:40:29 +0000

    cleaned up import statement and switched to use the logging util
    SVN-Revision: 1151

--- src/java/gov/nih/nci/ncicb/cadsr/util/DBConnectionUtils.java
+++ src/java/gov/nih/nci/ncicb/cadsr/util/DBConnectionUtils.java
@@ -1,6 +1,8 @@
 package gov.nih.nci.ncicb.cadsr.util;
 
 import gov.nih.nci.ncicb.cadsr.exception.JDBCConnectionException;
+import gov.nih.nci.ncicb.cadsr.util.logging.Log;
+import gov.nih.nci.ncicb.cadsr.util.logging.LogFactory;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
@@ -13,6 +15,7 @@ import javax.sql.DataSource;
 
 
 public class DBConnectionUtils {
+  private static Log log = LogFactory.getLog(DBConnectionUtils.class.getName());
   public static Connection getConnectionUsingUserInfo(ApplicationParameters ap)
     throws JDBCConnectionException {
     Connection conn = null;
@@ -41,16 +44,16 @@ public class DBConnectionUtils {
 
       DataSource ds = (DataSource) ic.lookup(dataSourceName);
       conn = ds.getConnection();
-      System.out.println(
+      log.info(
         "Connected to the database successfully using datasource " +
         dataSourceName);
     }
     catch (NamingException ne) {
-      ne.printStackTrace();
+      log.error("Failed to lookup JDBC datasource", ne);
       throw new JDBCConnectionException("Failed to lookup JDBC datasource", ne);
     }
     catch (Exception e) {
-      e.printStackTrace();
+      log.error("Exception occurred", e);
       throw new JDBCConnectionException("Unknown exception", e);
     }
 
@@ -67,16 +70,16 @@ public class DBConnectionUtils {
 
       DataSource ds = (DataSource) ic.lookup(dataSourceName);
       conn = ds.getConnection();
-      System.out.println(
+      log.info(
         "Connected to the database successfully using datasource " +
         dataSourceName);
     }
     catch (NamingException ne) {
-      ne.printStackTrace();
+      log.error("Failed to lookup JDBC datasource", ne);
       throw new JDBCConnectionException("Failed to lookup JDBC datasource", ne);
     }
     catch (Exception e) {
-      e.printStackTrace();
+      log.error("Exception occurred in getConnectionUsingDataSourceInfo", e);
       throw new JDBCConnectionException("Unknown exception", e);
     }
 
@@ -99,7 +102,7 @@ public class DBConnectionUtils {
     }
     catch (Exception e) {
       connectStatus = false;
-      e.printStackTrace();
+      log.error("Exception occurred in getConnectionUsingJDBC", e);
     }
 
     //return connectStatus;
