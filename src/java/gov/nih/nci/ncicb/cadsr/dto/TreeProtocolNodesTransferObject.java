7b8790c6373a8b3585d317fdf3ed7129b15e1c32
jiangja <jiangja>
2005-05-16 18:15:02 +0000

    TT#1526
    SVN-Revision: 1759

---
+++ src/java/gov/nih/nci/ncicb/cadsr/dto/TreeProtocolNodesTransferObject.java
@@ -0,0 +1,41 @@
+package gov.nih.nci.ncicb.cadsr.dto;
+import gov.nih.nci.ncicb.cadsr.resource.TreeParameters;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+public class TreeProtocolNodesTransferObject {
+
+ private Map formsWithNoProtocol;
+ private Map formsWithProtocol;
+ 
+ public TreeProtocolNodesTransferObject() {
+  
+  }
+
+
+  public void setFormsWithNoProtocol(Map formsWithNoProtocol)
+  {
+    this.formsWithNoProtocol = formsWithNoProtocol;
+  }
+
+
+  public Map getFormsWithNoProtocol()
+  {
+    return formsWithNoProtocol;
+  }
+
+
+  public void setFormsWithProtocol(Map formsWithProtocol)
+  {
+    this.formsWithProtocol = formsWithProtocol;
+  }
+
+
+  public Map getFormsWithProtocol()
+  {
+    return formsWithProtocol;
+  }
+
+
+}
