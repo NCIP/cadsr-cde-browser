ffb28460f49816be94bcf5725c47441d3daebd47
jiangj <jiangj>
2005-11-28 20:45:49 +0000

    clean imports
    SVN-Revision: 1962

--- src/java/gov/nih/nci/ncicb/cadsr/persistence/dao/BaseDAO.java
+++ src/java/gov/nih/nci/ncicb/cadsr/persistence/dao/BaseDAO.java
@@ -1,10 +1,8 @@
 package gov.nih.nci.ncicb.cadsr.persistence.dao;
 
-import gov.nih.nci.ncicb.cadsr.security.oc4j.BaseUserManager;
 import gov.nih.nci.ncicb.cadsr.servicelocator.ServiceLocator;
-
 import gov.nih.nci.ncicb.cadsr.util.logging.Log;
-import gov.nih.nci.ncicb.cadsr.util.logging.LogFactory;
+
 
 public abstract class BaseDAO {
   protected static Log log;
