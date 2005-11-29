78831c123bb3b58ef0603c85b612ff5a5bef4e4e
jiangj <jiangj>
2005-11-29 15:33:40 +0000

    initial version
    SVN-Revision: 1966

---
+++ src/java/gov/nih/nci/ncicb/cadsr/resource/impl/DefinitionImpl.java
@@ -0,0 +1,83 @@
+package gov.nih.nci.ncicb.cadsr.resource.impl;
+
+import gov.nih.nci.ncicb.cadsr.resource.ClassSchemeItem;
+import gov.nih.nci.ncicb.cadsr.resource.Context;
+import gov.nih.nci.ncicb.cadsr.resource.Definition;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+/**
+ * @author Jane Jiang (jane.jiang@oracle.com)
+ * @version $Id:
+ */
+public class DefinitionImpl implements Definition {
+    private String id;
+    private Context context;
+    private String definition;
+    private List csCsis;
+    private String type;
+    private String language;
+
+    public DefinitionImpl() {
+    }
+
+
+   public void setId(String anId) {
+      this.id = anId;
+   }
+
+   public String getId() {
+      return id;
+   }
+
+   public void setDefinition(String aDefinition) {
+      this.definition = aDefinition;
+   }
+
+   public String getDefinition() {
+      return definition;
+   }
+
+   public void setContext(Context newContext) {
+      this.context = newContext;
+   }
+
+   public Context getContext() {
+      return context;
+   }
+
+
+   public void setType(String _type) {
+      this.type = _type;
+   }
+
+   public String getType() {
+      return type;
+   }
+
+   public void setCsCsis(List csCsis) {
+      this.csCsis = csCsis;
+   }
+
+   public List getCsCsis() {
+      return csCsis;
+   }
+   
+   public void addCsCsi(ClassSchemeItem newCsCsi){
+      if (csCsis == null )
+         csCsis = new ArrayList();
+      
+      csCsis.add(newCsCsi);
+   
+   }
+   
+   public String getLanguage() {
+       return language;
+   }
+   
+   public void setLanguage(String newLanguage) {
+       language = newLanguage;
+   }
+}
