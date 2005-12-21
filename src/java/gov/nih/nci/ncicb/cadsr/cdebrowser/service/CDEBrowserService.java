8892530862172a8d9bd8d06395c087ba7547eacd
jiangj <jiangj>
2005-12-21 21:34:17 +0000

    TT#1628, added the service to get data element alt names and definitions
    SVN-Revision: 2054

--- src/java/gov/nih/nci/ncicb/cadsr/cdebrowser/service/CDEBrowserService.java
+++ src/java/gov/nih/nci/ncicb/cadsr/cdebrowser/service/CDEBrowserService.java
@@ -1,4 +1,7 @@
 package gov.nih.nci.ncicb.cadsr.cdebrowser.service;
+
+import gov.nih.nci.ncicb.cadsr.resource.DataElement;
+
 import java.util.Locale;
 import java.util.Properties;
 
@@ -6,4 +9,5 @@ public interface CDEBrowserService
 {
   public Properties getApplicationProperties(Locale locale);
   public Properties reloadApplicationProperties(Locale locale, String username);
+  public void populateDataElementAltNameDef (DataElement de);
 }
