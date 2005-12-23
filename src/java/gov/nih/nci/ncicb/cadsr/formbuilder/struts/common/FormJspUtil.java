3ab636bd3284a5ff4276c80b3bf7af94ac9cd31f
kakkodis <kakkodis>
2005-12-23 15:12:38 +0000

    [no comments]
    SVN-Revision: 2064

--- src/java/gov/nih/nci/ncicb/cadsr/formbuilder/struts/common/FormJspUtil.java
+++ src/java/gov/nih/nci/ncicb/cadsr/formbuilder/struts/common/FormJspUtil.java
@@ -1,10 +1,15 @@
 package gov.nih.nci.ncicb.cadsr.formbuilder.struts.common;
 
+import gov.nih.nci.ncicb.cadsr.resource.ClassSchemeItem;
 import gov.nih.nci.ncicb.cadsr.resource.Form;
 import gov.nih.nci.ncicb.cadsr.resource.FormElement;
 import gov.nih.nci.ncicb.cadsr.resource.Module;
 import gov.nih.nci.ncicb.cadsr.resource.Question;
 import gov.nih.nci.ncicb.cadsr.resource.FormValidValue;
+import gov.nih.nci.ncicb.cadsr.resource.Protocol;
+
+import java.util.Iterator;
+import java.util.List;
 
 public class FormJspUtil
 {
@@ -30,5 +35,37 @@ public class FormJspUtil
         return " ";
     }
     
+    public static String  getDelimitedProtocolLongNames(List protocols, String delimiter){
 
+          if (protocols==null || protocols.isEmpty()){
+              return "";
+          }
+          
+          StringBuffer sbuf = new StringBuffer();            
+          String delimtedProtocolLongName = null;
+          Iterator it = protocols.iterator();
+          while (it.hasNext()){
+              Protocol  p = (Protocol)it.next();
+               sbuf.append(delimiter).append(p.getLongName());
+          }
+          //System.out.println("subString = "  + sbuf.substring(1) );
+          return sbuf.substring(delimiter.length());
+        }
+        
+    public static String  getDelimitedCSILongNames(List classSchemeItems, String delimiter){
+
+          if (classSchemeItems==null || classSchemeItems.isEmpty()){
+              return "";
+          }
+          
+          StringBuffer sbuf = new StringBuffer();            
+          String delimtedProtocolLongName = null;
+          Iterator it = classSchemeItems.iterator();
+          while (it.hasNext()){
+              ClassSchemeItem  csi = (ClassSchemeItem)it.next();
+               sbuf.append(delimiter).append(csi.getClassSchemeItemName());
+          }
+
+          return sbuf.substring(delimiter.length());
+        }        
 }
