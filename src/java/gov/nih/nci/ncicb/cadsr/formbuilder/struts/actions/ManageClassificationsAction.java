a90eb9d1750cd2ce299d020f686615da557e3941
dongj <dongj>
2006-01-05 15:00:27 +0000

    update skip pattern when remove classifications
    SVN-Revision: 2108

--- src/java/gov/nih/nci/ncicb/cadsr/formbuilder/struts/actions/ManageClassificationsAction.java
+++ src/java/gov/nih/nci/ncicb/cadsr/formbuilder/struts/actions/ManageClassificationsAction.java
@@ -5,9 +5,12 @@ import gov.nih.nci.ncicb.cadsr.dto.ModuleTransferObject;
 import gov.nih.nci.ncicb.cadsr.dto.QuestionTransferObject;
 import gov.nih.nci.ncicb.cadsr.formbuilder.common.FormBuilderException;
 import gov.nih.nci.ncicb.cadsr.formbuilder.service.FormBuilderServiceDelegate;
+import gov.nih.nci.ncicb.cadsr.formbuilder.struts.common.FormActionUtil;
+import gov.nih.nci.ncicb.cadsr.formbuilder.struts.common.FormConstants;
 import gov.nih.nci.ncicb.cadsr.formbuilder.struts.formbeans.FormBuilderBaseDynaFormBean;
 import gov.nih.nci.ncicb.cadsr.jsp.bean.PaginationBean;
 import gov.nih.nci.ncicb.cadsr.resource.Form;
+import gov.nih.nci.ncicb.cadsr.resource.TriggerActionChanges;
 import gov.nih.nci.ncicb.cadsr.util.StringUtils;
 
 import org.apache.commons.logging.Log;
@@ -165,27 +168,39 @@ public class ManageClassificationsAction
     HttpServletResponse response) throws IOException, ServletException {
     DynaActionForm dynaForm = (DynaActionForm) form;
 
-    String[] ids = (String[]) dynaForm.get(CS_CSI_ID);
-    String id = ids[0];
+    /*String[] ids = (String[]) dynaForm.get(AC_CS_CSI_ID);//ascsiId
+    String acCsiId = ids[0];
+    */
+    String[] cscsiIds = (String[]) dynaForm.get(CS_CSI_ID);
+    String cscsiId = cscsiIds[0];
 
     try {
       Form crf = (Form) getSessionObject(request, CRF);
 
-      FormBuilderServiceDelegate service = getFormBuilderService();
-      service.removeFormClassification(id);
+      //check skip pattern
+      List<TriggerActionChanges> triggerChangesList = 
+                FormActionUtil.findFormSkipPatternForByClassificiation(crf, cscsiId);
+      if (triggerChangesList!=null && !triggerChangesList.isEmpty()){
+          setSessionObject(request, CLASSIFICATION_ASSOCIATED_TRIGGERS, triggerChangesList);
+          request.setAttribute(FormConstants.REMOVED_CLASSIFICATION_ID, cscsiId);
+          return mapping.findForward("hasSkipPattern");          
+      }
 
+      FormBuilderServiceDelegate service = getFormBuilderService();
+      service.removeFormClassification(cscsiId, crf.getFormIdseq());
+      
       Collection classifications =
         service.retrieveFormClassifications(crf.getFormIdseq());
 
+      //make sure there is no left-over triggers.
+      removeSessionObject(request, CLASSIFICATION_ASSOCIATED_TRIGGERS);
       setSessionObject(request, CLASSIFICATIONS, classifications);
     }
     catch (FormBuilderException exp) {
       if (log.isErrorEnabled()) {
         log.error("Exception on removeClassification ", exp);
       }
-
       saveError(exp.getErrorCode(), request);
-
       return mapping.findForward("failure");
     }
 
@@ -194,4 +209,42 @@ public class ManageClassificationsAction
     return mapping.findForward("success");
   }
     
+
+    public ActionForward updateSkipPatternForCSI(
+      ActionMapping mapping,
+      ActionForm form,
+      HttpServletRequest request,
+      HttpServletResponse response) throws IOException, ServletException {
+      
+      DynaActionForm formBean = (DynaActionForm)form;
+      String choice = (String)formBean.get("choice");//TODO: need to update the CRF for display
+      String cscsiId = (String)formBean.get(FormConstants.REMOVED_CLASSIFICATION_ID);
+      Form crf = (Form)getSessionObject(request, CRF);
+      try{
+          if ("yes".equalsIgnoreCase(choice)){
+              FormBuilderServiceDelegate service = getFormBuilderService();
+              List<TriggerActionChanges> triggerChangesList = (List<TriggerActionChanges>)
+                        getSessionObject(request,CLASSIFICATION_ASSOCIATED_TRIGGERS);
+              service.removeFormClassificationUpdateTriggerActions(cscsiId,  crf.getFormIdseq(), triggerChangesList);
+              
+              //update CRF in session
+              removeSessionObject(request, CRF);
+              String formId = crf.getFormIdseq();
+              crf = service.getFormDetails(formId);  
+              
+              Collection classifications = service.retrieveFormClassifications(formId);
+              setSessionObject(request, CLASSIFICATIONS, classifications);
+              setSessionObject(request, CRF, crf);
+          }
+          removeSessionObject(request,CLASSIFICATION_ASSOCIATED_TRIGGERS);
+      }catch(FormBuilderException e){
+          if (log.isErrorEnabled()) {
+            log.error("Exception on updateSkipPatternForCSI ", e);
+          }
+          saveError("cadsr.formbuilder.classification.updateSkipPattern.failure", request);
+          removeSessionObject(request,CLASSIFICATION_ASSOCIATED_TRIGGERS);
+          return mapping.findForward("failure");
+      }
+      return mapping.findForward(SUCCESS);
+      }
 }
