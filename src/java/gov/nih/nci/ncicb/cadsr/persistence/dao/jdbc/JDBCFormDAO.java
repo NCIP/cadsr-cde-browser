2e1a0516a9df62cc1375058fb40b266ddd30c7c1
dongj <dongj>
2006-02-13 21:17:58 +0000

    put back p_return_code for crf_version() stored procedure
    SVN-Revision: 2266

--- src/java/gov/nih/nci/ncicb/cadsr/persistence/dao/jdbc/JDBCFormDAO.java
+++ src/java/gov/nih/nci/ncicb/cadsr/persistence/dao/jdbc/JDBCFormDAO.java
@@ -195,7 +195,6 @@ public class JDBCFormDAO extends JDBCAdminComponentDAO implements FormDAO {
 
     Map out = nForm.execute(sourceFormId, newForm);
 
-    //if ((out.get("p_return_code")) == null) {
      if ( (out.get("p_return_desc")) == null) {    
       /*newForm.setFormIdseq((String) out.get("p_new_idseq"));
       return newForm;*/
@@ -222,8 +221,7 @@ public class JDBCFormDAO extends JDBCAdminComponentDAO implements FormDAO {
 
       Map out = vForm.execute(formIdSeq, newVersionNumber, changeNote, createdBy);
 
-      //if ((out.get("p_return_code")) == null) {
-       if ((out.get("p_return_desc")) == null) {
+      if ((out.get("p_return_code")) == null) {
         /*newForm.setFormIdseq((String) out.get("p_new_idseq"));
         return newForm;*/
         return (String) out.get("p_new_idseq");
@@ -1430,7 +1428,7 @@ public class JDBCFormDAO extends JDBCAdminComponentDAO implements FormDAO {
         declareParameter(new SqlParameter("p_Created_by", Types.VARCHAR));
 
         declareParameter(new SqlOutParameter("p_new_idseq", Types.VARCHAR));
-        //declareParameter(new SqlOutParameter("p_return_code", Types.VARCHAR));
+        declareParameter(new SqlOutParameter("p_return_code", Types.VARCHAR));
         declareParameter(new SqlOutParameter("p_return_desc", Types.VARCHAR));
         compile();
       }
