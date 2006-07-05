54b7b6385d79b64442cd3f7a77c52bd6e3cddd6f
dongj <dongj>
2006-07-05 17:54:17 +0000

    GF1224 - Lock forms to prevent users overwriting each other's changes while editing the same form added emailAddress and phoneNumber attributes
    SVN-Revision: 2409

--- src/java/gov/nih/nci/ncicb/cadsr/resource/NCIUser.java
+++ src/java/gov/nih/nci/ncicb/cadsr/resource/NCIUser.java
@@ -10,4 +10,7 @@ public interface NCIUser extends Serializable
   public Map getContextsByRole();
   public void setContextsByRole(Map contextsMap);
   public Collection getContextsByRoleAccess(String role);
+  //added for GF1224
+  public String getEmailAddress();
+  public String getPhoneNumber();
 }
