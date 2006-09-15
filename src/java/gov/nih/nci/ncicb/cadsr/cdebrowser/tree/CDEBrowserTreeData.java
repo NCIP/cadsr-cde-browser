f76883ca49dc9cb4b354afe5f9310bb6e4cc3cb0
jiangj <jiangj>
2006-09-15 16:26:42 +0000

    release 3.2 ,  cde browser tree code, added context preference, (training & test)
    SVN-Revision: 2484

--- src/java/gov/nih/nci/ncicb/cadsr/cdebrowser/tree/CDEBrowserTreeData.java
+++ src/java/gov/nih/nci/ncicb/cadsr/cdebrowser/tree/CDEBrowserTreeData.java
@@ -1,31 +1,30 @@
 package gov.nih.nci.ncicb.cadsr.cdebrowser.tree;
 
+import gov.nih.nci.ncicb.cadsr.cdebrowser.DataElementSearchBean;
 import gov.nih.nci.ncicb.cadsr.persistence.dao.AbstractDAOFactory;
 import gov.nih.nci.ncicb.cadsr.persistence.dao.ContextDAO;
 import gov.nih.nci.ncicb.cadsr.resource.Context;
 import gov.nih.nci.ncicb.cadsr.servicelocator.ApplicationServiceLocator;
-import gov.nih.nci.ncicb.cadsr.util.TreeUtils;
+import gov.nih.nci.ncicb.cadsr.util.SessionHelper;
 import gov.nih.nci.ncicb.webtree.ContextNode;
 import gov.nih.nci.ncicb.webtree.LazyActionTreeNode;
-
-import gov.nih.nci.ncicb.webtree.ProtocolFormTemplateNode;
-//import org.apache.catalina.connector.Request;
 
 import java.io.Serializable;
 
 import java.util.Collection;
 import java.util.Iterator;
 
-import java.util.Map;
-
 import javax.faces.context.FacesContext;
 
-import javax.servlet.ServletRequest;
+import javax.servlet.http.HttpSession;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
-import org.apache.myfaces.context.servlet.SessionMap;
 import org.apache.myfaces.custom.tree2.TreeModelBase;
+
+
+//import org.apache.catalina.connector.Request;
+
 
 public class CDEBrowserTreeData implements Serializable {
    private static final long serialVersionUID = 1L;
@@ -50,32 +49,38 @@ public class CDEBrowserTreeData implements Serializable {
          new LazyActionTreeNode("Context Folder", "caDSR Contexts",
              "javascript:classSearchAction('P_PARAM_TYPE=Context')",
              false);
+      boolean excludeTraining = true;
+      boolean excludeTest = true;
              
       Collection contexts = dao.getAllContexts();
       try {
       FacesContext facesContext = FacesContext.getCurrentInstance();
-      ServletRequest request =  (ServletRequest) facesContext.getExternalContext().getRequest();
-      String treeParam = request.getParameter("treeParams");
-      String contextToExclude =(String) TreeUtils.parseParameters(treeParam).get(TreeConstants.BR_CONTEXT_EXCLUDE_LIST_STR);
-
+      HttpSession session = (HttpSession) facesContext.getExternalContext().getSession(false);
+      DataElementSearchBean desb =(DataElementSearchBean) (SessionHelper.getInfoBean(session, "desb"));
+         if (desb != null) {
+            excludeTest = desb.isExcludeTestContext();
+            excludeTraining = desb.isExcludeTrainingContext();
+      }
+ 
       //first build a text folder node context.
 
-         for (Iterator iter = contexts.iterator(); iter.hasNext(); ) {
-            Context context = (Context)iter.next();
-            // if this context is not excluded by user preference
-            if (contextToExclude.indexOf(context.getName().toUpperCase()) <0) {
-            LazyActionTreeNode contextNode =
-               new ContextNode("Context Folder", context.getName() + " (" + context.getDescription() + ")",
-                 "javascript:performAction('P_PARAM_TYPE=CONTEXT&P_IDSEQ=" +
-                 context.getConteIdseq() +
-                 "&P_CONTE_IDSEQ=" +  context.getConteIdseq() +
-                 "&PageId=DataElementsGroup&NOT_FIRST_DISPLAY=1&performQuery=yes"
-                 +"')",
-                 context.getConteIdseq(),  false);
-            contextFolder.addLeaf(contextNode);
-            }
-
-          }
+      for (Iterator iter = contexts.iterator(); iter.hasNext(); ) {
+         Context context = (Context)iter.next();
+         // if this context is not excluded by user preference
+         if ((context.getName().equalsIgnoreCase("test") && excludeTest)
+         || (context.getName().equalsIgnoreCase("training") && excludeTraining))
+            continue;
+         
+         LazyActionTreeNode contextNode =
+            new ContextNode("Context Folder", context.getName() + " (" + context.getDescription() + ")",
+              "javascript:performAction('P_PARAM_TYPE=CONTEXT&P_IDSEQ=" +
+              context.getConteIdseq() +
+              "&P_CONTE_IDSEQ=" +  context.getConteIdseq() +
+              "&PageId=DataElementsGroup&NOT_FIRST_DISPLAY=1&performQuery=yes"
+              +"')",
+              context.getConteIdseq(),  false);
+         contextFolder.addLeaf(contextNode);
+       }
 
       } catch (Exception e) {
          log.error("Exception caught when building the tree", e);
