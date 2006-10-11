b6f2fa30a4217cca645f8b3e50b5106197608d5e
dongj <dongj>
2006-10-11 17:42:18 +0000

    GF1230-task409 added valueMeaning, formValueMeaningDesc, formValueMeaningText attributes
    SVN-Revision: 2510

--- src/java/gov/nih/nci/ncicb/cadsr/resource/FormValidValue.java
+++ src/java/gov/nih/nci/ncicb/cadsr/resource/FormValidValue.java
@@ -1,5 +1,7 @@
 package gov.nih.nci.ncicb.cadsr.resource;
 
+import java.util.List;
+
 public interface FormValidValue extends FormElement,Orderable,Instructionable {
   public String getValueIdseq();
   public void setValueIdseq(String idseq);
@@ -9,10 +11,19 @@ public interface FormValidValue extends FormElement,Orderable,Instructionable {
 
   public String getVpIdseq();
   public void setVpIdseq(String vpIdseq);
-  
-  
+
+
   public Object clone() throws CloneNotSupportedException ;
-  
+
   public String getShortMeaning();
   public void setShortMeaning(String shortMeaning);
+
+  //added for making value meaning an administered components;
+  public String getFormValueMeaningText();
+  public void setFormValueMeaningText(String vmText);
+  public String getFormValueMeaningDesc();
+  public void setFormValueMeaningDesc(String desc);
+  
+   public void setValueMeaning(ValueMeaning valueMeaning);
+   public ValueMeaning getValueMeaning();
 }
