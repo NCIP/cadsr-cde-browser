6a2f77561dd7b724de015d47a6eaf37f87661bbd
dongj <dongj>
2006-10-11 18:07:17 +0000

    GF1230-task409 new class
    SVN-Revision: 2526

---
+++ src/java/gov/nih/nci/ncicb/cadsr/dto/ValueMeaningTransferObject.java
@@ -0,0 +1,10 @@
+package gov.nih.nci.ncicb.cadsr.dto;
+
+import gov.nih.nci.ncicb.cadsr.resource.ClassSchemeItem;
+import gov.nih.nci.ncicb.cadsr.resource.Classification;
+import gov.nih.nci.ncicb.cadsr.resource.ValueMeaning;
+
+import java.util.List;
+
+public class ValueMeaningTransferObject extends AdminComponentTransferObject implements ValueMeaning{
+}
