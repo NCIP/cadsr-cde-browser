452202bdb2d81a13dadb75b2a78c0aca795127c2
dongj <dongj>
2006-10-11 17:43:35 +0000

    GF1230-task409 new class.
    SVN-Revision: 2512

---
+++ src/java/gov/nih/nci/ncicb/cadsr/resource/ValueMeaning.java
@@ -0,0 +1,9 @@
+package gov.nih.nci.ncicb.cadsr.resource;
+
+import java.util.List;
+
+/** added when ValueMeaning becomes an AdminComponent
+ * */
+ 
+public interface ValueMeaning extends AdminComponent{
+}
