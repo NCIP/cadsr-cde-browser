02be77994dcdbfd3936800334f07f272820107ac
jiangj <jiangj>
2006-10-14 02:47:22 +0000

    Release 3.2, GT# 1334, added contact information display for classification scheme
    SVN-Revision: 2532

---
+++ src/java/gov/nih/nci/ncicb/cadsr/resource/Organization.java
@@ -0,0 +1,29 @@
+package gov.nih.nci.ncicb.cadsr.resource;
+
+import java.util.Collection;
+
+public interface Organization {
+   public void setId(String id);
+
+   public String getId();
+
+   public void setName(String name) ;
+
+   public String getName() ;
+
+   public void setAddresses(Collection<Address> addresses);
+
+   public Collection<Address> getAddresses();
+
+   public void setPerson(Collection<Person> person) ;
+
+   public Collection<Person> getPerson();
+
+   public void setContacts(Collection<Contact> contacts) ;
+
+   public Collection<Contact> getContacts() ;
+   
+   public void setContactCommunications(Collection<ContactCommunication> contactCommunications);
+
+   public Collection<ContactCommunication> getContactCommunications() ;
+}
---
+++ src/java/gov/nih/nci/ncicb/cadsr/resource/Person.java
@@ -0,0 +1,46 @@
+package gov.nih.nci.ncicb.cadsr.resource;
+
+import java.util.Collection;
+
+public interface Person {
+   public void setFirstName(String firstName);
+
+   public String getFirstName() ;
+
+   public void setId(String id);
+
+   public String getId();
+
+   public void setLastName(String lastName);
+
+   public String getLastName();
+
+   public void setMiddleInitial(String middleInitial) ;
+
+   public String getMiddleInitial() ;
+
+   public void setPosition(String position);
+
+   public String getPosition() ;
+
+   public void setRank(Integer rank) ;
+
+   public Integer getRank() ;
+
+   public void setAddresses(Collection addresses);
+
+   public Collection getAddresses() ;
+
+   public void setContacts(Collection<Contact> contacts) ;
+
+   public Collection<Contact> getContacts() ;
+
+   public void setOrganization(Organization organization) ;
+
+   public Organization getOrganization() ;
+   
+   public void setContactCommunications(Collection<ContactCommunication> contactCommunications);
+
+   public Collection<ContactCommunication> getContactCommunications() ;
+   
+}
