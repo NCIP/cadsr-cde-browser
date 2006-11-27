ce5d450ddbb648fa7dfa0c3b049bd1b1b33ad5a2
dongj <dongj>
2006-11-27 18:27:19 +0000

    make it safe.
    SVN-Revision: 2614

--- src/java/gov/nih/nci/ncicb/cadsr/persistence/dao/jdbc/JDBCValueDomainDAO.java
+++ src/java/gov/nih/nci/ncicb/cadsr/persistence/dao/jdbc/JDBCValueDomainDAO.java
@@ -151,7 +151,7 @@ public class JDBCValueDomainDAO extends JDBCAdminComponentDAO implements ValueDo
           Object[] obj =
             new Object[] { shortMeaning };
           List ret =  execute(obj);
-          if (ret!=null){
+          if (ret!=null && (!ret.isEmpty()) ){
               return (ValueMeaning)(ret.get(0));
           }else{
               return null;
