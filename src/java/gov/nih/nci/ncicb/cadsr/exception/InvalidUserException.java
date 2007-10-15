92a6adeea4c3cdeb08b4a190fee527106de3cee1
hegdes <hegdes>
2007-10-15 18:24:11 +0000

    *** empty log message ***
    SVN-Revision: 2704

--- src/java/gov/nih/nci/ncicb/cadsr/exception/InvalidUserException.java
+++
@@ -1,9 +0,0 @@
-package gov.nih.nci.ncicb.cadsr.exception;
-
-public class InvalidUserException extends NestedRuntimeException 
-{
-  public InvalidUserException(String msg)
-  {
-    super(msg);
-  }
-}
